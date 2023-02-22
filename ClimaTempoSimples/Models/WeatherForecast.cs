4c76af9170dc3850e9a9d2935f306dafb6cf8d81
Sidney François <sidney.fvm.dev@outlook.com>
2023-02-22 14:19:31 -0300

    Add data annotations into WeatherForecast model to map database table to model.

--- ClimaTempoSimples/Models/WeatherForecast.cs
+++ ClimaTempoSimples/Models/WeatherForecast.cs
@@ -1,18 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace ClimaTempoSimples.Models
 {
+    [Table("PrevisaoClima")]
     public class WeatherForecast
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [Column("CidadeId")]
         public int CityId { get; set; }
         public City City { get; set; }
+
+        [Required]
+        [Column("DataPrevisao")]
         public DateTime ForecastDate { get; set; }
+
+        [Required]
+        [Column("Clima")]
+        [MaxLength(15)]
         public string Climate { get; set; }
+
+        [Required]
+        [Column("TemperaturaMaxima")]
         public float MinTemperature { get; set; }
+
+        [Required]
+        [Column("TemperaturaMinima")]
         public float MaxTemperature { get; set; }
     }
 }
