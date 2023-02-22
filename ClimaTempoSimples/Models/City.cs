73715e8d4c30283e7fcc1b589c6475e48214ab45
Sidney François <sidney.fvm.dev@outlook.com>
2023-02-22 14:15:19 -0300

    Add data annotations into City model to map database table to model object.

--- ClimaTempoSimples/Models/City.cs
+++ ClimaTempoSimples/Models/City.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace ClimaTempoSimples.Models
 {
+    [Table("Cidade")]
     public class City
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [Column("Nome")]
+        [MaxLength(200)]
         public string Name { get; set; }
+
+        [Required]
+        [Column("EstadoId")]
         public int StateId { get; set; }
         public State State { get; set; }
     }
