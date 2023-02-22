07e259b6f6993af15e98d834ead67bad3c426c16
Sidney François <sidney.fvm.dev@outlook.com>
2023-02-22 12:28:07 -0300

    Add Context class with specific connection string and data models.

---
+++ ClimaTempoSimples/Context/AppDataContext.cs
@@ -0,0 +1,20 @@
+﻿using ClimaTempoSimples.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ClimaTempoSimples.Context
+{
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext()
+            : base("AppConnectionString")
+        { }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+    }
+}
