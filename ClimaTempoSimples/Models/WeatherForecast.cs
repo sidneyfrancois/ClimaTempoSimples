6d2c1c2adb5d7a519e138f3f547e8d72fb510e28
Sidney François <sidney.fvm.dev@outlook.com>
2023-02-22 12:22:07 -0300

    Add basic WeatherForecast model.

---
+++ ClimaTempoSimples/Models/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClimaTempoSimples.Models
+{
+    public class WeatherForecast
+    {
+        public int Id { get; set; }
+        public int CityId { get; set; }
+        public DateTime ForecastDate { get; set; }
+        public string Climate { get; set; }
+        public float MinTemperature { get; set; }
+        public float MaxTemperature { get; set; }
+    }
+}
