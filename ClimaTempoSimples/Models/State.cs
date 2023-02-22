ac419dc5488544a5e86004151aae11e950a58445
Sidney François <sidney.fvm.dev@outlook.com>
2023-02-22 14:16:54 -0300

    Add data annotations into State model to map database table to model.

--- ClimaTempoSimples/Models/State.cs
+++ ClimaTempoSimples/Models/State.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace ClimaTempoSimples.Models
 {
+    [Table("Estado")]
     public class State
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [Column("Name")]
+        [MaxLength(200)]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(2)]
         public string UF { get; set; }
     }
 }
